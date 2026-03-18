b1679d42318e9737e8d147677f6f45b3d15c4c79
MrGodDavid <davidbodongliu@gmail.com>
2026-03-18 06:32:24 -0400

    Javadoc on ImageUtils.java

---
+++ src/ChoiceCraft_V1_0_6_Alpha/gfx/AnimationManager.java
@@ -0,0 +1,17 @@
+/**
+ * ========================================================================================================================
+ * Manages animation sheets of ChoiceCraft.
+ * <p>
+ * Author: David Liu.                                                                                   Date:3/18/2026
+ * ========================================================================================================================
+ */
+package ChoiceCraft_V1_0_6_Alpha.gfx;
+
+/**
+ * Manages animation sheets of ChoiceCraft.
+ *
+ * @author David Liu
+ * @since 3/17/2026
+ */
+public class AnimationManager {
+}
--- src/ChoiceCraft_V1_0_6_Alpha/gfx/SpriteLibrary.java
+++ src/ChoiceCraft_V1_0_6_Alpha/gfx/SpriteLibrary.java
@@ -24,11 +24,24 @@ public class SpriteLibrary {
 
     private Map<String, SpriteSet> units;
 
+    /**
+     * No-arg constructor of this class.
+     * <p>Precondition: none.</p>
+     * <p>Postcondition: create a new instance of unit Map, read all sprites from folders, and store them in sprite
+     * Map.</p>
+     */
     public SpriteLibrary() {
         units = new HashMap<String, SpriteSet>();
         loadSpritesFromDisk();
     }
 
+    /**
+     * Load sprites from disk. This method looks all folders in a specific resource path. For each folder in the resource
+     * folder, this method looks all the image resources and store each image into a SpriteSet, and finally, store each
+     * SpriteSet into the sprite Map.
+     * <p>Precondition: none.</p>
+     * <p>Postcondition: create the sprite Map with image resources from resource folder.</p>
+     */
     private void loadSpritesFromDisk() {
         String[] folderNames = getFolderNames(ENTITY_SPRITES_PATH);
 
@@ -48,12 +61,28 @@ public class SpriteLibrary {
         }
     }
 
+    /**
+     * Helper method that get all animation sheets from a specific folder.
+     * <p>Precondition: input file path is a String and is not null.</p>
+     * <p>Postcondition: get all animation sheets from the folder that specified by the basepath.</p>
+     *
+     * @param basePath that is not null.
+     * @return an array of names of all animation sheets in that folder.
+     */
     private String[] getSheetsInFolder(String basePath) {
         URL resource = SpriteLibrary.class.getResource(basePath);
         File file = new File(resource.getFile());
         return file.list((current, name) -> new File(current, name).isFile());
     }
 
+    /**
+     * Helper method that get all folders that contain animation sheets of ChoiceCraft from a specific folder.
+     * <p>Precondition: input file path is a String and is not null.</p>
+     * <p>Postcondition: get all folders that each contains animation sheets from that specified by the basepath.</p>
+     *
+     * @param basePath that is not null.
+     * @return an array of names of all folders that each contains animation sheets in that folder.
+     */
     private String[] getFolderNames(String basePath) {
         System.out.println(SpriteLibrary.class.getResource("/"));
         URL resource = SpriteLibrary.class.getResource(basePath);
